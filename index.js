e99cf07c2c5e8c1aab6aab4d45dd8e0e9102e19e
Supriyay <98298276+drocoder024@users.noreply.github.com>
2022-02-20 05:24:35 +0000

    options not restricted to just 4 and multiple arhs are'nt passed to function

--- index.js
+++ index.js
@@ -7,40 +7,35 @@ let score =0;
 let questionsList=[
   {
     question:'Which of the following keywords is used to define a variable in Javascript?',
-   option:{A:'var',B:'let',C:'both',D:'none'},
-   answer:'C'
+   option:['var','let','both','none'],
+   answer:'3'
   },
   {
     question:'How can a datatype be declared to be a constant type?',
-    option:{A:'var',B:'let',C:'final',D:'const'},
-    answer:'D'
+    option:['var','let','final','const'],
+    answer:'4'
   },
   {
     question:'What keyword is used to check whether a given property is valid or not?',
-    option:{A:'in',B:'is in',C:'exits',D:'lies'},
-    answer:'A'
+    option:['in','is in','exits','lies'],
+    answer:'1'
   },
    {
     question:`What's the type of null?`,
-    option:{A:'boolean',B:'undefined',C:'Object',D:'NAN'},
-    answer:'C'
-   },
+    option:['boolean','undefined','Object','NAN'],
+    answer:'3'
+  },
    {
     question:`output for print(NaN === NaN);`,
-    option:{A:'true',B:'undefined',C:'false',D:'error'},
-    answer:'C'
+    option:['true','undefined','false','error'],
+    answer:'3'
    }
 
 ]
 
-function play(question,option,answer)
+function checkScore(userInput,answer)
   {
-       console.log(question);
-       Object.keys(option).forEach(e=>console.log(`${e}: ${option[e]}`));
-    let userAnswer=readLineSync.question('Enter your answer as A or B or C or D\n');
-  
-  
-    if(userAnswer.toLocaleLowerCase()===answer.toLowerCase())
+    if(userInput.toLocaleLowerCase()===answer.toLowerCase())
     {
       console.log("you are right 📜 ☕");
       score++;
@@ -52,11 +47,24 @@ function play(question,option,answer)
     }
     console.log(`your current score is ${score}`);
      console.log("--------------------------");
-   
   }
 
 
-questionsList.forEach(ques=>play(ques.question,ques.option,ques.answer));
+function play(questionsList)
+  {
+      for(let i=0;i<questionsList.length;i++)
+        {
+          console.log(questionsList[i].question);
+          (questionsList[i].option).forEach((opt,index)=>console.log(`${index+1} ${opt}`));
+
+          const userInput=readLineSync.question('Enter your answer as 1 or 2 or 3 or 4\n');
+          checkScore(userInput,questionsList[i].answer);
+        }
+
+  }
+
+
+play(questionsList);
 console.log(`your total score is ${score}`)
 console.log("----------****Happy Quizzing****----------------");
 
